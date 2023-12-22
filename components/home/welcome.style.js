9e4cd9ed75e4ce476729f832410215ddaea4b88a
YismaelDiazH <105748001+YismaelDiazH@users.noreply.github.com>
2023-12-21 19:07:19 -0600

    Home screen Search Bar

--- components/home/welcome.style.js
+++ components/home/welcome.style.js
@@ -7,10 +7,46 @@ const styles = StyleSheet.create({
     },
     welcomeTxt: (color, top)=> ({
         fontFamily: "bold",
-        fontSize: SIZES.xxLarge -5,
+        fontSize: SIZES.xxLarge -6,
         marginTop: top,
         color: color,
-        marginHorizontal: 10
-    })
+        marginHorizontal: SIZES.small
+    }),
+    searchContainer:{
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        marginHorizontal: SIZES.small,
+        backgroundColor: COLORS.secondary,
+        borderRadius: SIZES.medium,
+        marginVertical: SIZES.medium,
+        height: 50
+    },
+    searchIcon: {
+        marginHorizontal: 10,
+        color: COLORS.gray
+    },
+    searchWrapper: {
+        flex: 1,
+        backgroundColor: COLORS.secondary,
+        marginRight: SIZES.small,
+        borderRadius: SIZES.small
+    },
+    searchInput: {
+        fontFamily: "regular",
+        width: "100%",
+        height: "100%",
+        paddingHorizontal: SIZES.small
+    },
+    searchBtn: {
+        width: 50,
+        backgroundColor: COLORS.primary,
+        height: "100%",
+        borderRadius: SIZES.medium,
+        justifyContent: "center",
+        alignItems: "center",
+        
+
+    }
 })
 export default styles
