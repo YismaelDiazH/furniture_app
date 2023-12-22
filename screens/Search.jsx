1ffb94ab54dd445dad518afe54a76081925dee5e
YismaelDiazH <105748001+YismaelDiazH@users.noreply.github.com>
2023-12-21 19:29:05 -0600

    Set up search bar in the Search page

--- screens/Search.jsx
+++ screens/Search.jsx
@@ -1,14 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { TouchableOpacity, TextInput, View } from 'react-native'
 import React from 'react'
+import { Feather, Ionicons } from '@expo/vector-icons';
 import { SafeAreaView } from 'react-native-safe-area-context'
+import styles from './search.style';
+import { SIZES, COLORS } from '../constants';
 const Search = () => {
   return (
     <SafeAreaView>
-      <Text>Home</Text>
+      <View style={styles.searchContainer}>
+        <TouchableOpacity>
+            
+            <Ionicons 
+            name="camera-outline" 
+            size={SIZES.xLarge} 
+            style={styles.searchIcon}
+            />
+        </TouchableOpacity>
+        <View style={styles.searchWrapper}>
+            <TextInput
+            style={styles.searchInput}
+            value=""
+            onPressIn={()=>{}}
+            placeholder="What are you looking for?"
+            />
+        </View>
+        <View>
+            <TouchableOpacity style={styles.searchBtn} >
+            <Feather name="search" size={24} style={styles.searchIcon}/>
+            </TouchableOpacity>
+        </View>
+      </View>
     </SafeAreaView>
   )
 }
 
 export default Search
-
-const styles = StyleSheet.create({})
