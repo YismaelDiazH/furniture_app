3eb2b98630d7af47d49d52f2deb620b48709e3fa
YismaelDiazH <105748001+YismaelDiazH@users.noreply.github.com>
2024-01-06 15:14:18 -0600

    Product card view finished

--- components/products/ProductCardView.jsx
+++ components/products/ProductCardView.jsx
@@ -1,13 +1,37 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import React from "react";
+import styles from "./productCardView.style";
+import { Ionicons } from "@expo/vector-icons";
+import { COLORS } from "../../constants";
 
 const ProductCardView = () => {
   return (
-    <TouchableOpacity onPress={()=>{}} >
-        <View></View>
-
+    <TouchableOpacity onPress={() => {}}>
+      <View style={styles.container}>
+          <View style={styles.imageContainer}>
+            <Image
+              source={{
+                uri: "https://images.pexels.com/photos/6707628/pexels-photo-6707628.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+              }}
+              style={styles.image}
+            ></Image>
+          </View>
+          <View style={styles.details}>
+            <Text style={styles.title} numberOfLines={1}>
+              Product
+            </Text>
+            <Text style={styles.supplier} numberOfLines={1}>
+              Product
+            </Text>
+            <Text style={styles.price}>$2344</Text>
+          </View>
+      
+        <TouchableOpacity style={styles.addBtn}>
+          <Ionicons name="add-circle" size={35} color={COLORS.primary} />
+        </TouchableOpacity>
+      </View>
     </TouchableOpacity>
-  )
-}
+  );
+};
 
-export default ProductCardView
+export default ProductCardView;
